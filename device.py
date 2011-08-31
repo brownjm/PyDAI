f3751cba4897c8e0d58db78074498e811adb38dc
Kyle <kt@Ceres.(none)>
2011-08-30 18:55:00 -0700

    Added initial code to handle device processing of commands.

--- device.py
+++ device.py
@@ -14,3 +14,42 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import threading
+import Queue
+import time, random
+
+class Device(object):
+    def __init__(self):
+        self.CommandPool = Queue.Queue()
+        self.CommandThread = ProcessThread(self)
+
+    def addCommand(self, command):
+        self.CommandPool.put(command)
+        if not self.CommandThread.isAlive():
+            self.CommandThread.start()
+
+class ProcessThread(threading.Thread):
+    def __init__(self, device):
+        self.device = device
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while not self.device.CommandPool.empty():
+            command = self.device.CommandPool.get()
+            #Do something with command here!
+            #Receive response from device and send back up chain.
+            time.sleep(random.randint(1,5))
+            print command            
+
+if __name__ == '__main__':
+    d = Device()
+    d.addCommand("Command1 Dev1")
+    d.addCommand("Command2 Dev1")
+    d.addCommand("Command3 Dev1")
+
+    a = Device()
+    a.addCommand("Command1 Dev2")
+    a.addCommand("Command2 Dev2")
+    a.addCommand("Command3 Dev2")
+
