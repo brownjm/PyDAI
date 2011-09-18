664e19d0cf6ac82d89b7cb8c39d078f74958ab6e
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2011-09-18 00:57:32 -0700

    Helper class can now supply information about command classes

--- executable.py
+++ executable.py
@@ -15,12 +15,13 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 from collections import deque
 import router
 import devicemanager
 import parser
 import env
-from constants import EXIT, EXEC, DEVMAN, ENV
+from constants import EXIT, EXEC, DEVMAN, ENV, HELP
 
 class Executable(router.Node):
     def __init__(self):
@@ -30,6 +31,7 @@ class Executable(router.Node):
         # create essential classes
         self.parser = parser.Parser(parser.commands, parser.rules)
         self.env = env.Environment()
+        self.helper = Helper(parser.commands)
         r = router.Router()
         devman = devicemanager.DeviceManager()
 
@@ -44,11 +46,16 @@ class Executable(router.Node):
         if line == EXIT:
             self.commands[EXIT]()
             return
-        try:
-            packet = self.parser.parse(line)
-            self.router.send(packet)
-        except parser.ParseError as pe:
-            print pe
+
+        elif HELP in line or "?" in line:
+            print self.helper.help(line)
+
+        else:
+            try:
+                packet = self.parser.parse(line)
+                self.router.send(packet)
+            except parser.ParseError as pe:
+                print pe
 
     def send(self, packet):
         raise Exception("Required to override")
@@ -70,20 +77,78 @@ class Executable(router.Node):
 
 
 class Helper(object):
-    """Object to give user access to doc strings within PyDAI."""
-    def __init__(self):
-        pass
+    """Gives user access to command docstrings within PyDAI."""
+    def __init__(self, commandDict):
+        """Gets docstrings from dictionary of command names and associated 
+classes."""
+        self.commandDict = {}
+        for name, commandClass in commandDict.iteritems():
+            self.commandDict[name] = self.trim(commandClass.__doc__)
 
-    def getCommandList(self):
+    def help(self, line):
+        """Get help on the line"""
+        if HELP in line:
+            words = line.split()
+            if len(words) == 1:
+                return self.helpmessage()
+            else:
+                commandName = words[1]
+                return self.getDocstring(commandName)
+        elif "?" in line:
+            words = line.split()
+            for word in words:
+                if word == "?":
+                    return self.helpmessage()
+                elif word.endswith("?"):
+                    commandName = word[:-1]
+                    return self.getDocstring(commandName)
+                else:
+                    return self.getDocstring(word)
+        else:
+            return line
+
+    def getCommands(self):
         """Return a list of available commands"""
-        pass
+        return self.commandDict.keys()
 
-    def getDocString(self, obj):
+    def getDocstring(self, name):
         """Return information specific to an object"""
-        pass
+        if name in self.commandDict:
+            return self.commandDict[name]
+        else:
+            return "No help for this command is available: {}".format(name)
 
-    def parse(self, line):
-        if "help" in line:
-            pass
-        if "?" in line:
-            pass
+    def helpmessage(self):
+        help = "Here are the available commands:\n{}\nTo receive more info on a command: help (command) or (command)?".format(self.getCommands())
+        return help
+
+    def trim(self, docstring):
+        """Removes newlines, aligns indentations, removes whitespace. 
+Code example from PEP257"""
+        if not docstring:
+            return ''
+        # Convert tabs to spaces (following the normal Python rules)
+        # and split into a list of lines:
+        lines = docstring.expandtabs().splitlines()
+        # Determine minimum indentation (first line doesn't count):
+        indent = sys.maxint
+        for line in lines[1:]:
+            stripped = line.lstrip()
+            if stripped:
+                indent = min(indent, len(line) - len(stripped))
+        # Remove indentation (first line is special):
+        trimmed = [lines[0].strip()]
+        if indent < sys.maxint:
+            for line in lines[1:]:
+                trimmed.append(line[indent:].rstrip())
+        # Strip off trailing and leading blank lines:
+        while trimmed and not trimmed[-1]:
+            trimmed.pop()
+        while trimmed and not trimmed[0]:
+            trimmed.pop(0)
+        # Return a single string:
+        return '\n'.join(trimmed)
+
+
+if __name__ == "__main__":
+    h = Helper(parser.commands)
