48c707902763f278385bc36de5a3a8ddaecdf146
Jeffrey M Brown <brown.jeffreym@gmail.com>
2012-07-09 13:43:13 -0700

    Added class that acts as a simplified interface to Python's builtin logging module

--- src/logger.py
+++ src/logger.py
@@ -14,3 +14,78 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Classes and function associated with handling log files for PyDAI."""
+
+import logging
+import atexit
+from constants import LOGFILE
+
+class Logger(object):
+    """Main class to log information from PyDAI that uses a simplified interface to the logging module."""
+    def __init__(self, logfilename=LOGFILE, level=3):
+        self.logfile = logfilename
+        self.running = False
+        self.divider = "-"*79 + "\n" # divider between logging sessions
+
+        # setup logging
+        logging.basicConfig(filename=self.logfile, format="%(asctime)s %(levelname)-8s%(message)s", level=logging.DEBUG)
+        self.level = level
+        self._start()
+        self.setLevel(level)
+
+    def _start(self):
+        """Manually start the logger"""
+        if not self.running:
+            self.running = True
+            self.write(self.divider)
+            logging.info("Start logging")
+            # register the stop method to be called upon exit of
+            # the Python interpreter
+            atexit.register(self._stop)
+
+            
+    def _stop(self):
+        """Manually stop the logger. The stop method has been registered with the atexit module, so it will automatically be called when the Python interpreter is shutting down."""
+        if self.running:
+            self.info("End logging")
+            self.write(self.divider)
+            self.running = False
+
+    def setLevel(self, level):
+        """Set the level of logging verbosity.
+        1 -> log errors only
+        2 -> log warnings and errors
+        3 -> log everything"""
+        if 1 <= level <= 3:
+            self.level = 3 # hack for printing following line
+            self.info("Logging level set to {}".format(level))
+            self.level = level # now set the actual level
+
+
+    def info(self, infoMessage):
+        """Place an information entry into the log file"""
+        if self.running and self.level == 3:
+            logging.info(infoMessage)
+
+    def warning(self, warningMessage):
+        """Place a warning entry into the log file"""
+        if self.running and self.level >= 2:
+            logging.warning(warningMessage)
+
+    def error(self, errorMessage):
+        """Place a warning entry into the log file"""
+        if self.running and self.level >= 1:
+            logging.error(errorMessage)
+
+    def write(self, text):
+        """Write text to the log file"""
+        with open(self.logfile, 'a') as f:
+            f.write(text)
+
+
+if __name__ == '__main__':
+    log = Logger()
+    log.info("Here is some great information")
+    log.warning("Something might not be correct")
+    log.error("It broke")
