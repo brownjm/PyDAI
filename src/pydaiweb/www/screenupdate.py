974d4bbbce1ba7c5045af0d37d56e136da17b612
KT <taylor.kyle.t@gmail.com>
2012-10-29 00:58:22 -0700

    Added process method to update the webecutable when windows are changed.

--- src/pydaiweb/www/screenupdate.py
+++ src/pydaiweb/www/screenupdate.py
@@ -2,6 +2,13 @@ from src.pydaiweb.bin.basepage import BasePage
 import json
 
 class screenupdate(BasePage):
+    def process(self):
+        if self.request['type'] == 'POST' and \
+        self.request['post'].has_key('window'):
+            scr = ''.join(['window=', \
+            self.request['post']['window'].value])
+            self.request['server'].webOutQueue.put(scr)
+
     def render(self):
         return dict(text=self.formatOutput())
         
